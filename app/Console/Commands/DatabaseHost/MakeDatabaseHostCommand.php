2aecd5f52a54ebd17baa0760b324ffb3c198aa0f
MartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-07-10 13:00:22 +0200

    Feature/dbhost cli alias (#4)
    * Add cli
    
    * Add alias
    
    * Update MakeDatabaseHostCommand.php
    
    * Update DatabaseHostListCommand.php

---
+++ app/Console/Commands/DatabaseHost/MakeDatabaseHostCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands\DatabaseHost;
+
+use Illuminate\Console\Command;
+use App\Services\Databases\Hosts\HostCreationService;
+
+class MakeDatabaseHostCommand extends Command
+{
+    protected $signature = 'p:databasehost:make
+                            {--name= : A name to identify the database host.}
+                            {--host= : The IP address that the database host should connect to create new databases}
+                            {--alias= : Enter the database host alias.}
+                            {--port= : Enter the database host port.}
+                            {--username= : Enter the database host username.}
+                            {--password= : Enter the database host password.}
+                            {--maxDatabases= : Enter the database host maximum number of databases.}';
+
+    protected $description = 'Creates a new database host on the system via the CLI.';
+
+    /**
+     * MakeDatabaseHostCommand constructor.
+     */
+    public function __construct(private HostCreationService $creationService)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Handle the command execution process.
+     *
+     * @throws \App\Exceptions\Model\DataValidationException
+     */
+    public function handle(): void
+    {
+        $data['name'] = $this->option('name') ?? $this->ask(__('commands.make_databasehost.name'));
+        $data['host'] = $this->option('host') ?? $this->ask(__('commands.make_databasehost.host'));
+        $data['alias'] = $this->option('alias') ?? $this->ask(__('commands.make_databasehost.alias'), '');
+        $data['port'] = $this->option('port') ?? $this->ask(__('commands.make_databasehost.port'), '3306');
+        $data['username'] = $this->option('username') ?? $this->ask(__('commands.make_databasehost.username'));
+        $data['password'] = $this->option('password') ?? $this->secret(__('commands.make_databasehost.password'));
+        $data['max_databases'] = $this->option('maxDatabases') ?? $this->ask(__('commands.make_databasehost.max_databases'));
+
+        $databaseHost = $this->creationService->handle($data);
+        $this->line(__('commands.make_databasehost.succes1') . $data['name'] . ' ' . __('commands.make_databasehost.succes2')  . $databaseHost->id . '.');
+    }
+}
